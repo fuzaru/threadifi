634868b77224bdfb1f6915d8eb94dc05caf9b90a
fuzaru <fuzareu@gmail.com>
2026-02-12 13:27:39 +0800

    update Elixir version requirement to 1.17.3 in mix.exs

--- mix.exs
+++ mix.exs
@@ -5,7 +5,7 @@ defmodule Threadifi.MixProject do
     [
       app: :threadifi,
       version: "0.1.0",
-      elixir: "~> 1.15",
+      elixir: "~> 1.17.3",
       elixirc_paths: elixirc_paths(Mix.env()),
       start_permanent: Mix.env() == :prod,
       aliases: aliases(),
